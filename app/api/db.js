74c328efc49b7a20215788478f047530d7a443c1
Ilia <ilia.loviagin@gmail.com>
2025-03-06 20:11:24 +0300

    Added users  route

--- app/api/db.js
+++ app/api/db.js
@@ -7,7 +7,7 @@ const connectDB = async () => {
     if (mongoose.connection.readyState >= 1) return;
 
     try {
-        await mongoose.connect(process.env.NEXT_PUBLIC_MONGO_URI);
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("✅ MongoDB подключен");
     } catch (err) {
         console.error("❌ Ошибка подключения к MongoDB:", err.message);
