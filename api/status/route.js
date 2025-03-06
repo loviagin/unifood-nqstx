1739e327a40cd09122a95e39761888c1018adc15
Ilia <ilia.loviagin@gmail.com>
2025-03-06 19:24:13 +0300

    Added status route

---
+++ api/status/route.js
@@ -0,0 +1,14 @@
+import { NextResponse } from "next/server";
+
+export async function GET(request) {
+  // Получаем параметры из URL
+  const { pathname } = request.nextUrl;
+  const id = pathname.split("/"); // Извлекаем username из URL
+
+  if (!id) {
+    return new NextResponse("Username not found", { status: 400 });
+  }
+
+  // Возвращаем редирект
+  return new NextResponse("Status correct", { status: 200 });
+}
